fc71f9e1a8c54934da30fa9f2a1ab7a8ac30dfd5
Ken <bgkenney@live.com>
2015-02-21 11:32:58 -0600

    Missing files

---
+++ src/REDCapClient/RCModel/Event.cs
@@ -0,0 +1,30 @@
+﻿namespace REDCapClient
+{
+    public class Event
+    {
+        private string _uniqueEventName;
+        private string _eventName;
+        private string _armNumber;
+
+        public string ArmNumber
+        {
+            get { return _armNumber; }
+            set { _armNumber = value; }
+        }
+
+
+        public string EventName
+        {
+            get { return _eventName; }
+            set { _eventName = value; }
+        }
+
+
+        public string UniqueEventName
+        {
+            get { return _uniqueEventName; }
+            set { _uniqueEventName = value; }
+        }
+
+    }
+}
