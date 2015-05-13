59098aef11f5a76aabec8fa9de12817e4fe9b9bb
Ken <bgkenney@live.com>
2015-05-13 13:24:48 -0500

    A little bit of records

---
+++ src/PCF.REDCap.Model/Record.cs
@@ -0,0 +1,12 @@
+﻿namespace PCF.REDCap
+{
+    public class Record
+    {
+        public string PatientId { get; set; }
+        public string EventDate { get; set; }
+        public string Concept { get; set; }        
+        public string ConceptValue { get; set; }
+        public string InstrumentName { get; set; }
+        public string EventName { get; set; }
+    }
+}
