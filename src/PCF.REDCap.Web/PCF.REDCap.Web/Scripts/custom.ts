583577917ffa9ca51d7a0258782c23bd45c875c9
Aaron-P <papp_a@kids.wustl.edu>
2015-05-26 12:33:43 -0500

    Move templates to script file. Work on dynamic modals. Cleanup.

--- src/PCF.REDCap.Web/PCF.REDCap.Web/Scripts/custom.ts
+++ src/PCF.REDCap.Web/PCF.REDCap.Web/Scripts/custom.ts
@@ -2,6 +2,7 @@
 /* tslint:disable:comment-format */
 
 /// <reference path="typings/jquery/jquery.d.ts" />
+/// <reference path="typings/knockout/knockout.d.ts" />
 /// <reference path="typings/bootstrap/bootstrap.d.ts" />
 /// <reference path="typings/nprogress/NProgress.d.ts" />
 /// <reference path="typings/toastr/toastr.d.ts" />
@@ -10,6 +11,7 @@
 interface Window {
 /* tslint:enable:interface-name */
     jQuery: JQueryStatic;
+    ko: KnockoutStatic;
     NProgress: NProgressStatic;
     toastr: Toastr;
     App: IApp;
@@ -22,8 +24,24 @@ interface JQueryStatic {
     event: any;
 };
 
+/* tslint:disable:interface-name */
+interface KnockoutStatic {
+/* tslint:enable:interface-name */
+    templates: { [name: string]: string };
+}
+
+//https://stackoverflow.com/questions/12766528/build-a-function-object-with-properties-in-typescript
+interface IModal {
+    (template: string, settings?: any): JQuery;
+    Defaults: any;
+};
+
 interface IApp {
     Nonce: () => string;
+    //Modal: { (template: string, settings?: any): JQuery; Defaults: any; };
+    //Modal: (template: string, settings?: any) => JQuery;
+    //ModalDefaults: any;
+    Modal: IModal;
     Ajax: (url: string, settings?: any) => JQueryXHR;
     Get: (url: string, data?: any, settings?: any) => JQueryXHR;
     Delete: (url: string, data?: any, settings?: any) => JQueryPromise<{}>;
@@ -32,7 +50,7 @@ interface IApp {
     Put: (url: string, data?: any, settings?: any) => JQueryPromise<{}>;
 };
 
-(function (window: Window, $: JQueryStatic, NProgress: NProgressStatic, toastr: Toastr): void {// TODO: Proper $ type jQueryStatic, need to handle extension unless we change it. (probably should)
+(function (window: Window, $: JQueryStatic, ko: KnockoutStatic, NProgress: NProgressStatic, toastr: Toastr): void {// TODO: Proper $ type jQueryStatic, need to handle extension unless we change it. (probably should)
     var App: IApp = window.App || <IApp>{};
 
     $(function (): void {
@@ -47,6 +65,72 @@ interface IApp {
             });
         });
     });
+
+    //#region jQuery Extensions
+
+    $.fn.applyBindings = function (viewModel: any): JQuery {
+        this.each(function (i: number, element: HTMLElement): void {
+            ko.applyBindings(viewModel, element);
+        });
+        return this;
+    };
+
+    //#endregion jQuery Extensions
+
+    //#region Knockout Templates
+
+    ko.templates["config-row"] =
+        "<tr data-bind=\"attr: { 'data-id': Id }\">" +
+            "<td class=\"break-block\" data-bind=\"text: Name\"></td>" +
+            "<td class=\"break-block\"><a href=\"#\" target=\"_blank\" data-bind=\"attr: { href: Url }, text: Url\"></a></td>" +
+            "<td class=\"break-block text-mono\" data-bind=\"text: Key\"></td>" +
+            "<td class=\"break-inline\"><button type=\"button\" class=\"btn btn-xs btn-block\" data-bind=\"click: Toggle, css: ob_Enabled() ? 'btn-success' : 'btn-danger', text: ob_Enabled() ? 'enabled' : 'disabled'\"></button></td>" +
+            "<td class=\"break-block text-nowrap\">" +
+                //"<button type=\"button\" class=\"btn btn-xs btn-warning\" title=\"Edit\" data-toggle=\"tooltip\" data-placement=\"left\" data-bind=\"click: Edit\"><span class="glyphicon glyphicon-edit"></span></button>" +
+                "<button type=\"button\" class=\"btn btn-xs btn-danger\" title=\"Delete\" data-toggle=\"tooltip\" data-placement=\"right\" data-bind=\"click: Delete\"><span class=\"glyphicon glyphicon-remove\"></span></button>" +
+            "</td>" +
+        "</tr>";
+
+    ko.templates["modal-config"] =
+        "<form class=\"_form-horizontal _form-bordered _form-modal\" role=\"form\" data-bind=\"submit: act_Submit, validate: {}\">" +
+            "<input name=\"Enabled\" type=\"hidden\" value=\"true\" />" +
+            "<div class=\"modal-header\">" +
+                "<button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\" aria-hidden=\"true\"><span class=\"glyphicon glyphicon-remove\"></span></button>" +
+                "<h4 class=\"modal-title\">Add REDCap Configuration</h4>" +
+            "</div>" +
+            "<div class=\"modal-body\">" +
+                "<div class=\"form-group\">" +
+                    "<div class=\"row\">" +
+                        "<div class=\"col-sm-12\">" +
+                            "<label for=\"Name\" class=\"control-label\">Name</label>" +
+                            "<input id=\"Name\" type=\"text\" name=\"Name\" placeholder=\"Example Config\" class=\"form-control\" maxlength=\"255\" data-bind=\"value: Name || ''\" required>" +
+                        "</div>" +
+                    "</div>" +
+                "</div>" +
+                "<div class=\"form-group\">" +
+                    "<div class=\"row\">" +
+                        "<div class=\"col-sm-12\">" +
+                            "<label for=\"Url\" class=\"control-label\">API Url</label>" +
+                            "<input id=\"Url\" type=\"url\" name=\"Url\" placeholder=\"https://redcap.example.com/api\" class=\"form-control\" maxlength=\"2048\" data-bind=\"value: Url || ''\" required>" +
+                        "</div>" +
+                    "</div>" +
+                "</div>" +
+                "<div class=\"form-group\">" +
+                    "<div class=\"row\">" +
+                        "<div class=\"col-sm-12\">" +
+                            "<label for=\"Key\" class=\"control-label\">API Key</label>" +
+                            "<input id=\"Key\" type=\"text\" name=\"Key\" placeholder=\"\" class=\"form-control\" maxlength=\"32\" pattern=\"[a-fA-f0-9]{32}\" data-bind=\"value: Key || ''\" required>" +
+                        "</div>" +
+                    "</div>" +
+                "</div>" +
+            "</div>" +
+            "<div class=\"modal-footer\">" +
+                "<button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Cancel</button>" +
+                "<button type=\"submit\" class=\"btn btn-primary\"><span class=\"glyphicon glyphicon-floppy-save\"></span> Save</button>" +
+            "</div>" +
+        "</form>";
+
+    //#endregion Knockout Templates
 
     //#region Misc
 
@@ -63,6 +147,52 @@ interface IApp {
     };
 
     //#endregion Misc
+
+    //#region Modals
+
+    App.Modal = <IModal>function (template: string, settings?: any): JQuery {
+        var html = ko.templates[template];
+        if (template === null)
+            return $();
+
+        //Deferred?
+        var defaults: Object = {
+            Size: '',//modal-lg for large or modal-sm for small, otherwise default.
+            Animate: true
+        };
+        var opts: any = $.extend({}, defaults, settings);
+
+        //Right now we only allow one modal to be open by closing any previous. Should we allow stacking? Should we fail the new one?
+        var $existing: JQuery = $('#modal-container');
+        if ($existing.hasClass('in')) {//Should we bother doing this check?
+            $existing.modal('hide');
+        } else {
+            $existing.remove();
+        }
+
+        var scroll: number;
+        var $modalContainer: JQuery = $(
+            '<div id="modal-container" class="modal ' + (opts.Animate ? 'fade' : '') + '" tabindex="-1" role="dialog" aria-hidden="true" aria-labelledby="modal-title">' +
+                '<div class="modal-dialog' + (opts.Size ? ' ' + opts.Size : '') + '">' +
+                    '<div class="modal-content">' + ko.templates[template] + '</div>' +
+                '</div>' +
+            '</div>')
+            .one('show.bs.modal', function () {
+                scroll = $('body').scrollTop();
+            })
+            .one('hidden.bs.modal', function (): void {
+                $(this).remove();
+                $('body').scrollTop(scroll);
+            });
+        return $modalContainer;
+    };
+
+    App.Modal.Defaults = {
+        backdrop: 'static',
+        keyboard: false
+    };
+
+    //#endregion Modals
 
     //#region Error Handlers
 
@@ -138,7 +268,7 @@ interface IApp {
             $.event.trigger("ajaxStart");
         }
 
-        var $iframe: JQuery = $("<iframe src='" + src + "' style='height: 0; width: 0; border: 0; padding: 0; margin: 0; position: absolute; top: 0; left: 0;'>")
+        var $iframe: JQuery = $("<iframe src=\"" + src + "\" style=\"height: 0; width: 0; border: 0; padding: 0; margin: 0; position: absolute; top: 0; left: 0;\">")
         .on("load", function (): void {
             var token: string = $(this).contents().find(input).val();
             if (token) {
@@ -229,7 +359,7 @@ interface IApp {
     .ajaxStop(function (): void {
         $("html").removeClass("busy");
         NProgress.done();
-    });
+        });
 
     window.App = App;
-} (window, window.jQuery, window.NProgress, window.toastr));
+} (window, window.jQuery, window.ko, window.NProgress, window.toastr));
